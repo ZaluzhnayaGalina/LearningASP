3b4bfff9aee1e02c8eb19d11999c79493d35a96c
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-01 01:17:18 +0300

    Show model on View

--- WebStore/WebStore/Controllers/HomeController.cs
+++ WebStore/WebStore/Controllers/HomeController.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private List<Employee> _employees = new List<Employee>
+        {
+            new Employee
+            {
+                Name = "Галина",
+                Patronymic="Александровная",
+                Surname="Залужная",
+                Age=23,
+                Id=0
+            },
+            new Employee
+            {
+                Name = "Наталья",
+                Patronymic = "Александровная",
+                Surname = "Наумова",
+                Age=26,
+                Id=1
+            }
+        };
         public IActionResult Index()
         {
-            return View();
+            return View(_employees);
         }
     }
 }
