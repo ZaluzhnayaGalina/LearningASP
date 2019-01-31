0b1b3e611bb4adfff80aa0d2fbebb99660265959
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-01 01:05:15 +0300

    Add employee model

---
+++ WebStore/WebStore/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebStore.Models
+{
+    public class Employee
+    {        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+    }
+}
