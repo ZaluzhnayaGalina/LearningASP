122d34a1622ce54115f6b3659aaced24fd68600a
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-01 00:38:51 +0300

    Add View for HomeController/Index

--- WebStore/WebStore/Controllers/HomeController.cs
+++ WebStore/WebStore/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebStore.Controllers
     {
         public IActionResult Index()
         {
-            return Content("Controller says Hello!");
+            return View();
         }
     }
 }
