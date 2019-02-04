1e418b74875235bb159db6568c4a953c65feac78
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-04 23:41:37 +0300

    Move employee-files to emloyee-folders

---
+++ WebStore/WebStore/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private List<Employee> _employees = new List<Employee>
+        {
+            new Employee
+            {
+                Name = "Галина",
+                Patronymic="Александровная",
+                Surname="Залужная",
+                Age=23,
+                Id=0,
+                Hobbies = new List<string>{"Вязание", "Матбои"}
+            },
+            new Employee
+            {
+                Name = "Наталья",
+                Patronymic = "Александровная",
+                Surname = "Наумова",
+                Age=26,
+                Id=1,
+                Hobbies = new List<string> {"Викторины"}
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(_employees);
+        }
+        public IActionResult Details(int id)
+        {
+            var employee = _employees.First(x => x.Id == id);
+            return View(employee);
+
+        }
+    }
+}
--- WebStore/WebStore/Controllers/HomeController.cs
+++ WebStore/WebStore/Controllers/HomeController.cs
@@ -9,36 +9,9 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private List<Employee> _employees = new List<Employee>
-        {
-            new Employee
-            {
-                Name = "Галина",
-                Patronymic="Александровная",
-                Surname="Залужная",
-                Age=23,
-                Id=0,
-                Hobbies = new List<string>{"Вязание", "Матбои"}
-            },
-            new Employee
-            {
-                Name = "Наталья",
-                Patronymic = "Александровная",
-                Surname = "Наумова",
-                Age=26,
-                Id=1,
-                Hobbies = new List<string> {"Викторины"}
-            }
-        };
         public IActionResult Index()
         {
-            return View(_employees);
-        }
-        public IActionResult Details(int id)
-        {
-            var employee = _employees.First(x => x.Id == id);
-            return View(employee);
-
+            return View();
         }
 
     }
