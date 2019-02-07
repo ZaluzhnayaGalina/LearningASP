7cc58c532794056cadb57c06ca07d98286af5f6e
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-08 01:24:54 +0300

    Fix id

--- WebStore/WebStore/Models/Employee.cs
+++ WebStore/WebStore/Models/Employee.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebStore.Models
 {
     public class Employee
     {        
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Surname { get; set; }
         public string Patronymic { get; set; }
         public int Age { get; set; }
