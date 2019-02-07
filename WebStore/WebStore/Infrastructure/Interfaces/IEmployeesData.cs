f4fc91813752f52eb4a825e9d35ca669c269bac8
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-08 00:17:41 +0300

    Interface to manipulate employees' data

---
+++ WebStore/WebStore/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WebStore.Models;
+namespace WebStore.Infrastructure.Interfaces
+{
+    interface IEmployeesData
+    {
+        IEnumerable<Employee> Get();
+        Employee GetEmployee(int id);
+        void AddEmployee(Employee newEmployee);
+        void Delete(int id);
+        void SaveChanges();
+    }
+}
