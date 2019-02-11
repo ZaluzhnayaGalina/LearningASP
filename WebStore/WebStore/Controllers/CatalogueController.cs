79c4e32a20b9f15a247e40eb2b2dce6d22a4ec40
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-12 00:56:15 +0300

    Create Catalogue Controller

---
+++ WebStore/WebStore/Controllers/CatalogueController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class CatalogueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Shop() => View();
+        public IActionResult ProductDetails() => View();
+    }
+}
