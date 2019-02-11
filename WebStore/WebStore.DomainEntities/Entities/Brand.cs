8d3d026be9da456454ca93d6b74ec36cd40383c0
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-12 01:23:11 +0300

    Add classes for brands and sections

---
+++ WebStore/WebStore.DomainEntities/Entities/Brand.cs
@@ -0,0 +1,10 @@
+﻿using WebStore.DomainEntities.Entities.Base;
+using WebStore.DomainEntities.Entities.Base.Interfaces;
+
+namespace WebStore.DomainEntities.Entities
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int OrderNumber { get; set ; }
+    }
+}
