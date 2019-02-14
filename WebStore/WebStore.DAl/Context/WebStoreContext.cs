39b979e6172dc0560d59e865456e9351721d8610
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-15 02:16:26 +0300

    Create WebStore dbContext

---
+++ WebStore/WebStore.DAl/Context/WebStoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.DomainEntities.Entities;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreContext:DbContext
+    {
+        public WebStoreContext(DbContextOptions options): base(options)
+        { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Section> Sections { get; set; }
+    }
+}
