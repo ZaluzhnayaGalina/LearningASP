223678df1381bfdc2c9b6943f2876aa4014921b7
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-24 23:45:09 +0300

    Add adminUser and roles

--- WebStore/WebStore.DomainEntities/Entities/User.cs
+++ WebStore/WebStore.DomainEntities/Entities/User.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace WebStore.DomainEntities.Entities
 {
     public class User : IdentityUser
     {
+        public const string UserRole = "User";
+        public const string AdminRole = "Admin";
+        public const string AdminUser = "Admin";
+        public const string TestUser = "TestUser";
     }
 }
