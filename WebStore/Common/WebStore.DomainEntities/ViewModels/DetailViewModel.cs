51d3d47c3e540b474f5b97b0f5f95e33396649c6
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-03-02 13:21:43 +0300

    Move ViewModels to DomainEntities

---
+++ WebStore/Common/WebStore.DomainEntities/ViewModels/DetailViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebStore.DomainEntities.ViewModels
+{
+    public class DetailsViewModel
+    {
+        public CartViewModel CartViewModel { get; set; }
+
+        public OrderViewModel OrderViewModel { get; set; }
+    }
+}
